2b965f3f27526aefae80ef8e8b5fe233cba4e9c2
GiannisMeg <megoulas2@hotmail.com>
2022-09-06 21:43:56 +0200

    endpoint details/post comment

--- routers/cocktails.js
+++ routers/cocktails.js
@@ -2,19 +2,19 @@ const { Router } = require("express");
 
 //model
 const Cocktail = require("../models").cocktail;
-const Comment = require("../models").comment;
-const Review = require("../models").review;
 
 const router = new Router();
 
 // Get All cocktails
 router.get("/", async (req, res, next) => {
+	// console.log("req.body", req.body);
 	try {
 		const allCocktails = await Cocktail.findAll();
 
 		if (!allCocktails) {
-			res.status(404).send("no cocktail found");
+			res.status(404).send("no cocktails found");
 		}
+
 		res.json(allCocktails);
 	} catch (err) {
 		next(err);
@@ -26,11 +26,10 @@ router.get("/:id", async (req, res, next) => {
 	const { id } = req.params;
 
 	try {
-		const cocktailWithDetails = await Cocktail.findByPk(id, {
-			include: { model: Comment },
-		});
+		const cocktailWithDetails = await Cocktail.findByPk(id);
 
 		res.json(cocktailWithDetails);
+		// console.log("id", cocktailWithDetails);
 	} catch (err) {
 		next(err);
 	}
